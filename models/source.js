9e8cef3a7f7bd2d3f1656450c203dfb2706a881e
d-mv <d.dash.mv@gmail.com>
2019-03-25 15:45:36 +0200

    Moving to universal URL for API, added Create Source action

--- models/source.js
+++ models/source.js
@@ -26,122 +26,135 @@ const SourceSchema = mongoose.Schema({
 const Source = (module.exports = mongoose.model("Source", SourceSchema));
 
 module.exports.getSourceByName = (name, callback) => {
-  console.log(`Source.getSourceByName: ${name}`);
+  console.log("\x1b[32m", `~ Source.getSourceByName: ${name}`);
   const query = { name: name };
   Source.findOne(query, callback);
 };
 
-module.exports.createSource = (newSource, callback) => {
-  newSource.save(callback);
+module.exports.createSource = (fields, callback) => {
+  Source.getSourceByName(fields.name, (err, res) => {
+    if (err) callback(err);
+    if (res) {
+      console.log("\x1b[34m", "~ source exists");
+      return null
+    } else {
+      console.log(
+        "\x1b[32m",
+        `~ Creating new source with name: ${fields.name}`
+      );
+      const newSource = new Source(fields);
+      newSource.save(callback);
+    }
+  });
 };
 module.exports.getListOfSources = (req, callback) => {
   Source.find({}, callback);
 };
 
-// const parserOptions = {
-//   attributeNamePrefix: "@_",
-//   attrNodeName: "attr", //default is 'false'
-//   textNodeName: "#text",
-//   ignoreAttributes: true,
-//   ignoreNameSpace: false,
-//   allowBooleanAttributes: false,
-//   parseNodeValue: true,
-//   parseAttributeValue: false,
-//   trimValues: true,
-//   cdataTagName: "__cdata", //default is 'false'
-//   cdataPositionChar: "\\c",
-//   localeRange: "", //To support non english character in tag/attribute values.
-//   parseTrueNumberOnly: false,
-//   attrValueProcessor: a => he.decode(a, { isAttributeValue: true }), //default is a=>a
-//   tagValueProcessor: a => he.decode(a) //default is a=>a
+// // const parserOptions = {
+// //   attributeNamePrefix: "@_",
+// //   attrNodeName: "attr", //default is 'false'
+// //   textNodeName: "#text",
+// //   ignoreAttributes: true,
+// //   ignoreNameSpace: false,
+// //   allowBooleanAttributes: false,
+// //   parseNodeValue: true,
+// //   parseAttributeValue: false,
+// //   trimValues: true,
+// //   cdataTagName: "__cdata", //default is 'false'
+// //   cdataPositionChar: "\\c",
+// //   localeRange: "", //To support non english character in tag/attribute values.
+// //   parseTrueNumberOnly: false,
+// //   attrValueProcessor: a => he.decode(a, { isAttributeValue: true }), //default is a=>a
+// //   tagValueProcessor: a => he.decode(a) //default is a=>a
+// // };
+
+// const createPost = (source, post) => {
+//   const newPost = new Post({
+//     source: source,
+//     title: post.title.__cdata,
+//     url: post.link,
+//     author: post["dc:creator"].__cdata,
+//     published: post.pubDate,
+//     parsed: new Date(),
+//     text: post["content:encoded"].__cdata
+//   });
+//   newPost.save(err => {
+//     if (err) console.log(err);
+//   });
 // };
 
-const createPost = (source, post) => {
-  const newPost = new Post({
-    source: source,
-    title: post.title.__cdata,
-    url: post.link,
-    author: post["dc:creator"].__cdata,
-    published: post.pubDate,
-    parsed: new Date(),
-    text: post["content:encoded"].__cdata
-  });
-  newPost.save(err => {
-    if (err) console.log(err);
-  });
-};
+// const proccessPosts = (source, posts) => {
+//   // console.log(source);
+//   // newSource = {
+//   //   name: "freeCodeCamp",
+//   //   url: "https://medium.freecodecamp.org/feed",
+//   //   home: "https://medium.freecodecamp.org"
+//   // };
+//   // const createNewSource = new Source(newSource);
+//   // createNewSource.save();
+//   // console.log(createNewSource);
+//   // 5c950392ccb6ae09507f6da3
 
-const proccessPosts = (source, posts) => {
-  // console.log(source);
-  // newSource = {
-  //   name: "freeCodeCamp",
-  //   url: "https://medium.freecodecamp.org/feed",
-  //   home: "https://medium.freecodecamp.org"
-  // };
-  // const createNewSource = new Source(newSource);
-  // createNewSource.save();
-  // console.log(createNewSource);
-  // 5c950392ccb6ae09507f6da3
+//   posts.forEach(post => {
+//     createPost(source, post);
+//   });
+//   // Post.getPostById("5c9509bfb21ff60d537ef96f",(err,res)=> {
+//   // console.log(res.title);
 
-  posts.forEach(post => {
-    createPost(source, post);
-  });
-  // Post.getPostById("5c9509bfb21ff60d537ef96f",(err,res)=> {
-  // console.log(res.title);
+//   // });
+// };
 
-  // });
-};
+// module.exports.updatePosts = (source, callback) => {
+// // parse the server response
+//   console.log(`Source.updatePosts: ${source}`);
 
-module.exports.updatePosts = (source, callback) => {
-// parse the server response
-  console.log(`Source.updatePosts: ${source}`);
+//   const parseResponse = (source, response) => {
+//     const xmlData = response.data;
 
-  const parseResponse = (source, response) => {
-    const xmlData = response.data;
+//     if (parser.validate(xmlData) === true) {
+//       //optional (it'll return an object in case it's not valid)
+//       var jsonObj = parser.parse(xmlData, parserOptions);
+//     }
+//     // Intermediate obj
+//     var tObj = parser.getTraversalObj(xmlData, parserOptions);
+//     var jsonObj = parser.convertToJson(tObj, parserOptions);
+//     // parse and refill posts
+//     proccessPosts(source, jsonObj.rss.channel.item);
+//     // newPost.save(err => {
+//     //   if (err) console.log(err);
+//     // });
+//     return "";
+//   };
 
-    if (parser.validate(xmlData) === true) {
-      //optional (it'll return an object in case it's not valid)
-      var jsonObj = parser.parse(xmlData, parserOptions);
-    }
-    // Intermediate obj
-    var tObj = parser.getTraversalObj(xmlData, parserOptions);
-    var jsonObj = parser.convertToJson(tObj, parserOptions);
-    // parse and refill posts
-    proccessPosts(source, jsonObj.rss.channel.item);
-    // newPost.save(err => {
-    //   if (err) console.log(err);
-    // });
-    return "";
-  };
-
-  async function getSourceDetails() {
-    // set url var
-    let url = "";
-    let sourceId = "";
-    // find the source
-    await Source.findOne({ name: source })
-      .catch(e => {
-        console.log(e);
-      })
-      .then(response => {
-        url = response.url;
-        sourceId = response._id;
-        console.log(`Got sourceID: ${sourceId}`);
-      });
-    // set result var
-    let result = "";
-    // get the source data
-    axios
-      .get(url)
-      .then(res => {
-        result = res;
-      })
-      .catch(err => {
-        console.log(err);
-      })
-      .then(() => {
-        callback(null, parseResponse(sourceId, result));
-      });
-  }
-  getSourceDetails();
-};
+//   async function getSourceDetails() {
+//     // set url var
+//     let url = "";
+//     let sourceId = "";
+//     // find the source
+//     await Source.findOne({ name: source })
+//       .catch(e => {
+//         console.log(e);
+//       })
+//       .then(response => {
+//         url = response.url;
+//         sourceId = response._id;
+//         console.log(`Got sourceID: ${sourceId}`);
+//       });
+//     // set result var
+//     let result = "";
+//     // get the source data
+//     axios
+//       .get(url)
+//       .then(res => {
+//         result = res;
+//       })
+//       .catch(err => {
+//         console.log(err);
+//       })
+//       .then(() => {
+//         callback(null, parseResponse(sourceId, result));
+//       });
+//   }
+//   getSourceDetails();
+// };
--- routes/sources.js
+++ routes/sources.js
@@ -13,38 +13,71 @@ router.get("/list", function(req, res, next) {
   });
 });
 
-router.get("/create", function(req, res, next) {
-  Source.getListOfSources(req, (err, response) => {
-    if (err) throw err;
-    res.send(response);
-  });
+router.post("/", function(req, res, next) {
+  const area = req.body.action[0]
+  const action = req.body.action[1]
+  const id = req.body.id
+  const fields = req.body.fields
+  if (area === 'source') {
+    switch (action){
+      case 'create':
+      if (fields) {
+        // call for create source method
+        Source.createSource(fields, (err, response) => {
+          if (err) throw err;
+          res.send(response);
+        });
+      }
+      break;
+      case 'update':
+      if (id && fields){
+        // call for update method
+      }
+      break;
+      case 'delete':
+      if (id){
+        // call for delete method
+      }
+      break;
+      case 'list':
+      // return list
+        Source.getListOfSources(req, (err, response) => {
+          if (err) throw err;
+          res.json(response);
+        });
+      break;
+      default:
+      res.send(null)
+      break
+    }
+  }
 });
 
-router.get("/delete", function(req, res, next) {
-  Source.getListOfSources(req, (err, response) => {
-    if (err) throw err;
-    res.send(response);
-  });
-});
-router.get("/update", function(req, res, next) {
-  console.log("Updating posts");
-  const request = "freeCodeCamp";
-  Source.updatePosts(request, (err, response) => {
-    if (err) throw err;
-    res.send(response);
-  });
-});
-router.get("*", function(req, res, next) {
-  const request = req.path.substr(1);
-  Source.getListOfSources(request, (err, response) => {
-    if (err) throw err;
-    console.log(`SourceID: ${response._id}`);
-    const sourceId = response._id;
-    Post.getPostsBySource(sourceId, (err, response) => {
-      if (err) throw err;
-      res.send(response);
-    });
-  });
-});
+// router.get("/delete", function(req, res, next) {
+//   Source.getListOfSources(req, (err, response) => {
+//     if (err) throw err;
+//     res.send(response);
+//   });
+// });
+// router.get("/update", function(req, res, next) {
+//   console.log("Updating posts");
+//   const request = "freeCodeCamp";
+//   Source.updatePosts(request, (err, response) => {
+//     if (err) throw err;
+//     res.send(response);
+//   });
+// });
+// router.get("/tobcontinued", function(req, res, next) {
+//   const request = req.path.substr(1);
+//   Source.getListOfSources(request, (err, response) => {
+//     if (err) throw err;
+//     console.log(`SourceID: ${response._id}`);
+//     const sourceId = response._id;
+//     Post.getPostsBySource(sourceId, (err, response) => {
+//       if (err) throw err;
+//       res.send(response);
+//     });
+//   });
+// });
 
 module.exports = router;
