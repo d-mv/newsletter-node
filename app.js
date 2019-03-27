1848b48ba0a5e79e6dae2a59bb9e0bd2c19430c0
d-mv <d.dash.mv@gmail.com>
2019-03-27 10:49:52 +0200

    Added cors, removed mercury

--- app.js
+++ app.js
@@ -1,39 +1,41 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require("http-errors");
+const express = require("express");
+const cors = require("cors");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
 const mongo = require("mongodb");
 const mongoose = require("mongoose");
 const db = mongoose.connection;
-const dotenv = require("dotenv").config()
-
+const dotenv = require("dotenv").config();
 
 // routes
-var indexRouter = require('./routes/index');
-// var sourcesRouter = require('./routes/sources');
-// var postsRouter = require('./routes/posts');
+const indexRouter = require("./routes/index");
 
-var app = express();
+const app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 
-app.use(logger('dev'));
+app.use(logger("dev"));
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
 // Access Control
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
+// app.use((req, res, next) => {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header(
+//     "Access-Control-Allow-Headers",
+//     "Origin, X-Requested-With, Content-Type, Accept"
+//   );
+//   next();
+// });
 
-app.use('/', indexRouter);
+app.use("/", indexRouter);
 // app.use('/sources', sourcesRouter);
 // app.use('/posts', postsRouter);
 
@@ -46,11 +48,11 @@ app.use(function(req, res, next) {
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
 
 module.exports = app;
